2c8dfca2dc7c9576fdce4f8432e6da71ef5a3a55
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-09-17 19:05:47 -0500

    home base

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonList,
+  IonItem,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonList,
+    IonItem,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonCol,
+    RouterModule,
+    CommonModule,
+  ],
 })
+
 export class HomePage {
   constructor() {}
 }
