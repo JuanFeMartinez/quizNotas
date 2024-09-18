85755babc7266a4db0a45e89c05ad10ca578dca7
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-09-17 19:46:50 -0500

    Menu agregado y home actualizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,13 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonMenu,
+  IonButtons,
+  IonMenuButton,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+  IonText,
 } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -34,9 +41,49 @@ import { CommonModule } from '@angular/common';
     IonCol,
     RouterModule,
     CommonModule,
+    IonMenu,
+    IonButtons,
+    IonMenuButton,
+    IonLabel,
+    IonSelect,
+    IonSelectOption,
+    IonText,
   ],
 })
+export class HomePage implements OnInit {
+  semestres: string[] = ['2023-1', '2023-2', '2024-1', '2024-2'];
+  semestreSeleccionado: string = '';
+  materias: any[] = [];
 
-export class HomePage {
   constructor() {}
+
+  ngOnInit() {
+
+    this.semestreSeleccionado = this.semestres[0];
+    this.cargarMaterias(this.semestreSeleccionado);
+  }
+
+
+  cambiarSemestre(event: any) {
+    this.semestreSeleccionado = event.detail.value;
+    this.cargarMaterias(this.semestreSeleccionado);
+  }
+
+ 
+  cargarMaterias(semestre: string) {
+
+    if (semestre === '2023-1') {
+      this.materias = [
+        { nombre: 'Matemáticas', notas: [] },
+        { nombre: 'Física', notas: [] },
+      ];
+    } else if (semestre === '2023-2') {
+      this.materias = [
+        { nombre: 'Química', notas: [] },
+        { nombre: 'Biología', notas: [] },
+      ];
+    } else {
+      this.materias = []; 
+    }
+  }
 }
